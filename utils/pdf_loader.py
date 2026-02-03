7a8bfd0839ed84bb8dbfce69676eaaa04125bf2b
Harish Kumar N P <nphk1609@gmail.com>
2026-02-03 09:38:59 +0530

    Changed to PyMuPDF

--- utils/pdf_loader.py
+++ utils/pdf_loader.py
@@ -1,12 +1,30 @@
-from pypdf import PdfReader
+import fitz  
 
 def get_pdf_text(uploaded_file):
     try:
-        reader = PdfReader(uploaded_file)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text() + "\n"
-        return text
+        file_stream = uploaded_file.read()
+        doc = fitz.open(stream=file_stream, filetype="pdf")
+        
+        full_content = ""
+        extracted_links = []
+
+        for page in doc:
+           
+            full_content += page.get_text() + "\n"
+            
+            links = page.get_links()
+            for link in links:
+                if "uri" in link:
+                    extracted_links.append(link["uri"])
+        
+        doc.close()
+
+        
+        if extracted_links:
+            full_content += "\n--- EXTRACTED LINKS ---\n"
+            full_content += "\n".join(set(extracted_links))
+
+        return full_content
     except Exception as e:
-        print(f"Error parsing PDF: {e}")
+        print(f"Error parsing PDF with links: {e}")
         return ""
